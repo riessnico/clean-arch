df658dc18205ec603c1aa5b6d91ed642004fbe58
Nicolau Riess <nicolauriess@gmail.com>
2021-12-14 00:16:18 -0300

    fix: changed folder view settings

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from './protocols/http/http-post-client'
+import { HttpPostClient } from '../../protocols/http/http-post-client'
 import { RemoteAuthentication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
