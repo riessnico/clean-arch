3e7e02a58110f7364f793e936c58e1c328dac06c
Nicolau Riess <nicolauriess@gmail.com>
2021-12-15 00:22:01 -0300

    fix: typo in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,50 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main/index.tsx',
-    output: {
-        path: path.join(__dirname, 'public/js'),
-        publicPath: '/public/js',
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extension: ['.ts', '.tsx', '.js', 'scss'],
-        alias: {
-            '@': path.join(__dirname, 'src')
-        }
-    },
-    module: {
-        rules: [{
-            test: /\.ts(x?)$/,
-            loader: 'ts-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.scss$/,
-            use: [{
-                loader: 'style-loader'
-            }, {
-                loader: 'css-loader',
-                options: {
-                    module: true
-                }
-            }, {
-                loader: 'sass-loader'
-            }]
-        }]
-    },
-    devServer: {
-        static: './public',
-        historyApiFallback: true,
-        devMiddleware: {
-            writeToDisk: true,
-        }
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
   },
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDom'
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ]
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          module: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
+  devServer: {
+    static: './public',
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDom'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
