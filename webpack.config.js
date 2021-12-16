7fd7d5b26da8dc057ec7acc5a73a4e603dae4a51
Nicolau Riess <nicolauriess@gmail.com>
2021-12-15 23:05:58 -0300

    fix: webpack typo

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       }, {
         loader: 'css-loader',
         options: {
-          module: true
+          modules: true
         }
       }, {
         loader: 'sass-loader'
