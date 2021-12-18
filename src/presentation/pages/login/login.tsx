99c4db85a607c681c1f34ab8edc45bd71bff00da
Nicolau Riess <nicolauriess@gmail.com>
2021-12-18 14:23:15 -0300

    feat: ensure login with inputs with required state

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,25 +4,25 @@ import {
   Footer,
   Input,
   FormStatus,
-  LoginHeader
+  LoginHeader,
 } from '@/presentation/components/'
 import FormContext from '@/presentation/contexts/form/form-context'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
+  const [state] = useState({
     isLoading: false,
-    errorMessage: ''
+  })
+
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: '',
   })
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <FormContext.Provider value={state}>
+      <FormContext.Provider value={{ state, errorState }}>
         <form action="" className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu email" />
@@ -31,7 +31,12 @@ const Login: React.FC = () => {
             name="password"
             placeholder="Digite sua senha"
           />
-          <button disabled data-testid="submit" className={Styles.submit} type="submit">
+          <button
+            disabled
+            data-testid="submit"
+            className={Styles.submit}
+            type="submit"
+          >
             Entrar
           </button>
           <span className={Styles.link}>Criar conta</span>
