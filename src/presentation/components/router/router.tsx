49be518d38653fbd380ffa731bd53f1b2d074a41
Nicolau Riess <nicolauriess@gmail.com>
2022-01-08 17:42:47 -0300

    fix: changing to react router dom new template

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/login" exact component={Login} />
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   )
 }
