a8faa9130780fc3782c7f4f22149a69b79916ed5
Nicolau Riess <nicolauriess@gmail.com>
2022-03-27 20:00:29 -0300

    refactor: changing router file according to router dom version

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Login } from '@/presentation/pages';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 };
